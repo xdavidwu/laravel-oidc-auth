0ad8049f24ae6c8c56e3d1f3f1074acc8f8f7a94
xdavidwu <xdavidwuph@gmail.com>
2021-10-24 20:55:54 +0800

    CallbackController: handle errors

--- src/CallbackController.php
+++ src/CallbackController.php
@@ -17,10 +17,18 @@ class CallbackController extends Controller
 
     public function callback(Request $request)
     {
+        $error = $request->get('error');
+        if (!is_null($error)) {
+            return response('OIDC error: ' . $error, 400);
+        }
+
         if ($request->get('state') !== session('oidc-auth.state')) {
             return response('state does not match', 400);
         }
 
+        if (!$request->has('code')) {
+            return response('No authorization code received', 400);
+        }
         $token = $this->provider->getAccessToken('authorization_code', [
             'code' => $request->get('code'),
         ]);
