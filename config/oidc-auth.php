94c013e612356ed6375785e6ecc3da82e51fec58
xdavidwu <xdavidwuph@gmail.com>
2021-10-24 11:41:44 +0800

    config: document configs

--- config/oidc-auth.php
+++ config/oidc-auth.php
@@ -1,6 +1,16 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | OIDC Provider Config
+    |--------------------------------------------------------------------------
+    |
+    | This options to pass to OpenIDConnectClient\OpenIDConnectProvider.
+    | `redirectUri` will be determined automatically by `callback_route` below.
+    |
+     */
     'provider' => [
         'clientId' => 'example',
         'clientSecret' => 'example',
@@ -8,7 +18,17 @@ return [
         'urlAuthorize' => 'http://example.com/authorize',
         'urlAccessToken' => 'http://example.com/token',
         'urlResourceOwnerDetails' => 'http://example.com/owner',
+        'scopes' => ['openid'],
         'publicKey' => 'file:///key.pem',
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Callback Route
+    |--------------------------------------------------------------------------
+    |
+    | Callback route used by Authrization Code flow.
+    |
+     */
     'callback_route' => '/oidc/callback',
 ];
