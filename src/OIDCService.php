843ed6981a402ee8903ac32de3c936adf2345f7d
Pinghao Wu <xdavidwuph@gmail.com>
2023-09-23 21:39:37 +0800

    OIDCService: add helper to get a valid access_token

--- src/OIDCService.php
+++ src/OIDCService.php
@@ -43,4 +43,31 @@ class OIDCService
     {
         return session(self::TOKEN_SESSION_KEY);
     }
+
+    public function getToken()
+    {
+        $token = $this->getStoredToken();
+        if ($token->hasExpired()) {
+            $newToken = $this->provider->getAccessToken('refresh_token', [
+                'refresh_token' => $token->getRefreshToken(),
+            ]);
+            /**
+             * TODO steverhoades/oauth2-openid-connect-client currently assumes
+             * id_token is present on every grant, but oidc spec allow it to be
+             * absent on refresh.
+             *
+             * after steverhoades/oauth2-openid-connect-client#40 lands, this
+             * may breaks our built-in oidc-auth-session auth provider. we may
+             * hack around it by detecting and patch in old id_token. we may
+             * also consider using this in oidc-auth-session to always ensure a
+             * valid token? (actually, considering id_token expiration for that
+             * is more correct)
+             */
+            $this->storeToken($newToken);
+
+            return $newToken;
+        } else {
+            return $token;
+        }
+    }
 }
